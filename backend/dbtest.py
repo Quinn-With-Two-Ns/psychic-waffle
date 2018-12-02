93a09c004a8c4536eae4f41112148f432efba177
andrewmlaw <andrewmlaw@gmail.com>
2018-12-01 18:56:25 -0800

    Connecting OCR api to backend server

--- backend/dbtest.py
+++ backend/dbtest.py
@@ -1,7 +1,21 @@
 import db
+import api
 
-dbconn = db.DbHandler('test.db')
+mode = 2
 
-#dbconn.add_user('Simong')
-dbconn.add_restriction('test restriction', 'allergy')
+if mode == 0:
+    dbconn = db.DbHandler('test.db')
 
+    #dbconn.add_user('Simong')
+    dbconn.add_restriction('test restriction', 'allergy')
+
+elif mode == 1:
+    cmd = r'/usr/bin/tesseract'
+    nutrition = api.read_label(cmd, 'juice.jpg')
+    print(nutrition)
+
+elif mode == 2:
+    dbconn = db.DbHandler('test.db')
+
+    #dbconn.add_user('Simong')
+    dbconn.add_consumption(1, 'juice.jpg')
